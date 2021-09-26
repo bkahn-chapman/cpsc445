99bdae213f5e7ab5dd8e891e389e4227099ce55a
kahnbenjamina <bkahn@chapman.edu>
2021-09-26 13:42:58 -0700

    Removed unnecessary features.

--- life.cpp
+++ life.cpp
@@ -3,7 +3,36 @@
 #include <fstream>
 #include <cmath>
 #include <string>
+#include <cstdlib>
 using namespace std;
+
+class life
+{
+  public:
+    life(); //default constructor
+    ~life(); //destructor
+
+    //check each possible cell position
+    int CheckCornerTL(int row, int column);
+    int CheckCornerTR(int row, int column);
+    int CheckCornerBL(int row, int column);
+    int CheckCornerBR(int row, int column);
+    int CheckSideT(int row, int column);
+    int CheckSideR(int row, int column);
+    int CheckSideB(int row, int column);
+    int CheckSideL(int row, int column);
+    int CheckMiddle(int row, int column);
+
+    int numRows; //number of rows in the map
+    int numColumns; //number of columns in the map
+    int genNum; //tracks the current generation number
+
+    void NewGen(); //generates the next map and sends the old one to the previous map
+    void PrintMap(string outputFile); //prints the current map
+   
+    char** previousMap; //old map
+    char** currentMap; //new map
+};
 
 //class constructor
 life::life()
@@ -17,7 +46,7 @@ life::~life()
 
 }
 
-/counts and returns all of the possible neighbors for the cell in the top left corner (takes in the position of the cell)
+//counts and returns all of the possible neighbors for the cell in the top left corner (takes in the position of the cell)
 int life::CheckCornerTL(int row, int column)
 {
   int numAround = 0;
@@ -247,7 +276,6 @@ void life::NewGen()
   {
     for(int j = 0; j < numColumns; ++j)
     {
-      oscillateMap[i][j] = previousMap[i][j]; //sets the previous map to the second previous map
       previousMap[i][j] = currentMap[i][j]; //sets the current map to the previous map
     }
   }
@@ -341,103 +369,9 @@ void life::NewGen()
   }
 }
 
-//prints the current map (takes in the user's print option choice and the file they wish to output the results to)
-void life::PrintMap(string choice, string outputFile)
+//outputs the current map (takes in the file the user wishes to output the results to)
+void life::PrintMap(string outputFile)
 {
-  //if the user wants print option pause
-  if(choice == "pause")
-  {
-    //pauses for .5 seconds every time
-    usleep(500000);
-    //if this is the original map
-    if(genNum == 0)
-    {
-      cout << "0" << endl;
-      //prints the map
-      genNum++;
-      for(int i = 0; i < numRows; ++i)
-      {
-        for(int j = 0; j < numColumns; ++j)
-        {
-          cout << currentMap[i][j];
-        }
-        cout << endl;
-      }
-      cout << "Generation Number: 1" << endl;
-    }
-    //if this is any simulated generation
-    else
-    {
-      cout << "Generation Number: " << genNum << endl;
-    }
-
-    //prints the map
-    for(int i = 0; i < numRows; ++i)
-    {
-      for(int j = 0; j < numColumns; ++j)
-      {
-        cout << currentMap[i][j];
-      }
-      cout << endl;
-    }
-    genNum++; //increments the generation counter
-  }
-  //if the user wants to use the enter print option
-  else if(choice == "enter")
-  {
-    int enterCheck = 0; //checks if the user inputted enter
-
-    //if at the created map
-    if(genNum != 0 || genNum != 1)
-    {
-      //cotninues to check until the user presses enter
-      while(enterCheck == 0)
-      {
-        cout << "Press enter to continue." << endl;
-        //if the input is an enter
-        if(cin.get() == '\n')
-        {
-          enterCheck = 1; //exit the while loop
-        }
-      }
-    }
-
-    //if at the created map
-    if(genNum == 0)
-    {
-      cout << "0" << endl;
-      //prints the map
-      genNum++;
-      for(int i = 0; i < numRows; ++i)
-      {
-        for(int j = 0; j < numColumns; ++j)
-        {
-          cout << currentMap[i][j];
-        }
-        cout << endl;
-      }
-      cout << "Generation Number: 1" << endl;
-    }
-    //if at a generated map
-    else
-    {
-      cout << "Generation Number: " << genNum << endl;
-    }
-
-    //prints the map
-    for(int i = 0; i < numRows; ++i)
-    {
-      for(int j = 0; j < numColumns; ++j)
-      {
-        cout << currentMap[i][j];
-      }
-      cout << endl;
-    }
-
-    genNum++; //increments the generation counter
-  }
-  else if(choice == "write")
-  {
     //opens the file output to the file name of the user's choice
     ofstream outFS;
     outFS.open(outputFile, ios::app);
@@ -475,49 +409,14 @@ void life::PrintMap(string choice, string outputFile)
     }
     genNum++; //increments the generation counter
     outFS.close(); //closes the file output
-  }
 }
-
-class life
-{
-  public:
-    life(); //default constructor
-    ~life(); //destructor
-
-    void RandomMap(int rows, int columns, float cellFreq); //creates a map for a random gen
-    void GivenMap(int rows, int columns, string fileName); //creates a map for a given gen
-
-    //check each possible cell position
-    int CheckCornerTL(int row, int column);
-    int CheckCornerTR(int row, int column);
-    int CheckCornerBL(int row, int column);
-    int CheckCornerBR(int row, int column);
-    int CheckSideT(int row, int column);
-    int CheckSideR(int row, int column);
-    int CheckSideB(int row, int column);
-    int CheckSideL(int row, int column);
-    int CheckMiddle(int row, int column);
-
-    int numRows; //number of rows in the map
-    int numColumns; //number of columns in the map
-    int genNum; //tracks the current generation number
-
-    void NewGen(); //generates the next map and sends the old one to the previous map
-    void PrintMap(string choice, string outputFile); //prints the current map
-    bool CheckValid(); //checks if the generations should continue
-
-    char** oscillateMap; //map before the previous map
-    char** previousMap; //old map
-    char** currentMap; //new map
-};
 
 int main(int argc, char** argv)
 {
     string inputFile = argv[1];
     string outputFile = argv[2];
-    int numSteps = argv[3];
-    int numThreads = argv[4];
-
+    int numSteps = atoi(argv[3]);
+    int numThreads = atoi(argv[4]);
 
     cout << "Welcome to Conway's Game of Life!" << endl;
 }
