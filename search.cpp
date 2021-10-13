46667fc22144d7cb0ea686815403edc3f9305085
kahnbenjamina <bkahn@chapman.edu>
2021-10-12 23:49:54 -0700

    Comments!

--- search.cpp
+++ search.cpp
@@ -1,3 +1,11 @@
+/*
+* Name: Benjamin Kahn
+* ID: 2316793
+* Class: CPSC-445-01
+* Assignment: Assignment 2
+* About: Finds how many times each of a list of given words occurs in a given set of text with a given number of threads. Outputs to a given file.
+*/
+
 #include <algorithm>
 #include <fstream>
 #include <iostream>
@@ -7,37 +15,39 @@
 #include <vector>
 using namespace std;
 
+// Defines all of the public variables to be used throughout the program (especially the totals vector).
 class Program
 {
     public:
-        vector<int> totals;
-        void wordCounter(vector<string> words, vector<string> sentence, int numThreads, int threadNum);
-        void checkArgs(int numArgs);
-        void checkInput(string fileName);
-        vector<string> readFiles(string fileName);
-        void validOutput(string fileName);
-        void checkThreads(string numThreads);
-        void inputValidity(string keywords, string texttoread, string outputFile, string numThreads);
-        int maxThreads(string threads, vector<string> sentences);
-        void runProgram(vector<string> words, vector<string> sentences, int numThreads, string outputFile);
+        vector<int> totals; //tracks the total number of times each word is found accross all threads/lines
+        void wordCounter(vector<string> words, vector<string> sentence, int numThreads, int threadNum); //counts each word on a per-thread basis
+        void checkArgs(int numArgs); //checks to see if the proper number of arguments is given
+        void checkInput(string fileName); //checks if the input file given exists
+        vector<string> readFiles(string fileName); //reads through the given files to create string vectors
+        void validOutput(string fileName); //checks if the given output file is valid
+        void checkThreads(string numThreads); //checks if the number of threads given is valid
+        void inputValidity(string keywords, string texttoread, string outputFile, string numThreads); //combines all checks into one function for main
+        int maxThreads(string threads, vector<string> sentences); //ensures there aren't more threads than lines in the input file
+        void runProgram(vector<string> words, vector<string> sentences, int numThreads, string outputFile); //runs the program and outputs results
 };
 
+//counts each word on a per-thread basis
 void Program::wordCounter(vector<string> words, vector<string> sentence, int numThreads, int threadNum)
 {
-    for(int s = 0; s < sentence.size(); ++s)
+    for(int s = 0; s < sentence.size(); ++s) //checks every sentence in the given input file
     {
-        if(s % numThreads == threadNum)
+        if(s % numThreads == threadNum) //cyclical distribution of threads
         {
             stringstream ss(sentence[s]);
-            while(ss)
+            while(ss) //while there are words in the sentence
             {
                 string word;
                 ss >> word;
-                for(int w = 0; w < words.size(); ++w)
+                for(int w = 0; w < words.size(); ++w) //checks each given keyword
                 {
-                    if(word == words[w])
+                    if(word == words[w]) 
                     {
-                        totals[w]++;
+                        totals[w]++; //adds to that word's vector position if it is found
                     }
                 }
             }
@@ -45,21 +55,22 @@ void Program::wordCounter(vector<string> words, vector<string> sentence, int num
     }
 }
 
-
+//checks to see if the proper number of arguments is given
 void Program::checkArgs(int numArgs)
 {
-    if((numArgs-1) != 4)
+    if((numArgs-1) != 4) //makes sure there are exactly four arguments given
     {
         cout << "invalid arguemnts (expected 4 arguments, instead received " << numArgs-1 << ")" << endl;
         exit(1);
     }
 }
 
+//checks if the input file given exists
 void Program::checkInput(string fileName)
 {
     ifstream inFS;
     inFS.open(fileName);
-    if(!inFS)
+    if(!inFS) //checks if the given file exists
     {
         cout << "One or more of the input files does not exist." << endl;
         exit(1);
@@ -67,21 +78,23 @@ void Program::checkInput(string fileName)
     inFS.close();
 }
 
+//reads through the given files to create string vectors
 vector<string> Program::readFiles(string fileName)
 {
-    vector<string> tobeFilled;
+    vector<string> tobeFilled; //creates the vector that will be used in later steps
     string l;
     ifstream inFS;
     inFS.open(fileName);
-    while(getline(inFS, l))
+    while(getline(inFS, l)) //iterates through every line in the given file
     {
-        l.erase(remove_if(l.begin(), l.end(), ::ispunct), l.end());
-        tobeFilled.push_back(l);
+        l.erase(remove_if(l.begin(), l.end(), ::ispunct), l.end()); //removes punctuation
+        tobeFilled.push_back(l); //pushes each line to the end of the vector
     }
     inFS.close();
-    return tobeFilled;
+    return tobeFilled; //the string vector
 }
 
+//checks if the given output file is valid
 void Program::validOutput(string fileName)
 {
     string textFile = ".txt"; //to check if the output file is valid
@@ -93,8 +106,10 @@ void Program::validOutput(string fileName)
     }
 }
 
+//checks if the number of threads given is valid
 void Program::checkThreads(string numThreads)
 {
+    //makes sure every thread is a whole number
     for(char const &c : numThreads)
     {
         if(isdigit(c) == 0)
@@ -113,17 +128,20 @@ void Program::checkThreads(string numThreads)
     }
 }
 
+//combines all checks into one function for main
 void Program::inputValidity(string keywords, string texttoread, string outputFile, string numThreads)
 {
-    checkInput(keywords);
-    checkInput(texttoread);
-    validOutput(outputFile);
-    checkThreads(numThreads);
+    checkInput(keywords); //checks if the keyword file is valid
+    checkInput(texttoread); //checks if the text to read file is valid
+    validOutput(outputFile); //checks if the given output file is valid
+    checkThreads(numThreads); //checks if the given number of threads is valid
 }
 
+//ensures there aren't more threads than lines in the input file
 int Program::maxThreads(string threads, vector<string> sentences)
 {
     int intThreads = stoi(threads);
+    //prevents there from being more threads than lines in the file as it would leave unused threads
     if(intThreads > sentences.size())
     {
         intThreads = sentences.size();
@@ -131,14 +149,17 @@ int Program::maxThreads(string threads, vector<string> sentences)
     return intThreads;
 }
 
+//runs the program and outputs results
 void Program::runProgram(vector<string> words, vector<string> sentences, int numThreads, string outputFile)
 {
+    //generates the initial values for the vector of word totals
     for(int w = 0; w < words.size(); ++w)
     {
         totals.push_back(0);
     }
-    sort(words.begin(), words.end());
+    sort(words.begin(), words.end()); //sorts the words alphabetically
     thread* myThreads = new thread[numThreads]; //creates the new threads
+    //sends each of the threads to count the words in its rows (cyclical distribution)
     for(int t = 0; t < numThreads; ++t)
     {
         myThreads[t]=thread(&Program::wordCounter, this, words, sentences, numThreads, t);
@@ -147,6 +168,7 @@ void Program::runProgram(vector<string> words, vector<string> sentences, int num
     delete[] myThreads;
     ofstream outFS;
     outFS.open(outputFile, ios::app);
+    //outputs the results to the given file
     for(int i = 0; i < totals.size(); ++i)
     {
         outFS << words[i] << " " << totals[i] << endl;
@@ -156,15 +178,15 @@ void Program::runProgram(vector<string> words, vector<string> sentences, int num
 
 int main(int argc, char** argv)
 {  
-    Program prog;
-    prog.checkArgs(argc);
-    string keywords = argv[1]; //takes in the file to be read
-    string texttoread = argv[2];
+    Program prog; //new instance of the program to have global access to the totals vector
+    prog.checkArgs(argc); //checks if the correct number of argumebts is given
+    string keywords = argv[1]; //takes in the file of keywords
+    string texttoread = argv[2]; //takes in the file of text to be read through
     string outputFile = argv[3]; //takes in the file to be written to
-    string threadInput = argv[4];
-    prog.inputValidity(keywords, texttoread, outputFile, threadInput);
-    vector<string> words = prog.readFiles(keywords);
-    vector<string> sentences = prog.readFiles(texttoread);
-    int numThreads = prog.maxThreads(threadInput, sentences);
-    prog.runProgram(words, sentences, numThreads, outputFile);    
+    string threadInput = argv[4]; //takes in the desired number of threads
+    prog.inputValidity(keywords, texttoread, outputFile, threadInput); //checks if all given arguments are valid
+    vector<string> words = prog.readFiles(keywords); //generates the list of keywords to look for
+    vector<string> sentences = prog.readFiles(texttoread); //generates the list of lines from the given file to be looked through
+    int numThreads = prog.maxThreads(threadInput, sentences); //corrects for if there are more threads than lines
+    prog.runProgram(words, sentences, numThreads, outputFile); //runs the program now that everything is confirmed to be correct
 }
