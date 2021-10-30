e50dc9f127d1f7ee037cb89c87ec3a7ee6ff18c6
kahnbenjamina <bkahn@chapman.edu>
2021-10-29 22:52:02 -0700

    Added new 'dna_count.cpp.

---
+++ dna_count.cpp
@@ -0,0 +1,29 @@
+#include <mpi.h>
+#include <iostream>
+
+using namespace std;
+
+void check_error(int status, const string message="MPI error") {
+  if ( status != 0 ) {    
+    cerr << "Error: " << message << endl;
+    exit(1);
+  }
+}
+
+int main (int argc, char *argv[]) {
+  int rank;
+  int p;
+
+  // Initialized MPI
+  check_error(MPI_Init(&argc, &argv), "unable to initialize MPI");
+  check_error(MPI_Comm_size(MPI_COMM_WORLD, &p), "unable to obtain p");
+  check_error(MPI_Comm_rank(MPI_COMM_WORLD, &rank), "unable to obtain rank");
+
+  if(rank == 0)
+  {
+    cout << "rank: " << rank << endl;
+  }
+  check_error(MPI_Finalize());
+
+  return 0;
+}
