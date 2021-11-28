41a521cd54533e139145cf69b759f486ef311e52
kahnbenjamina <bkahn@chapman.edu>
2021-11-28 14:28:39 -0800

    Testing input.

--- dna_invert.cu
+++ dna_invert.cu
@@ -12,5 +12,11 @@ int main () {
     string dna;
     getline(inFS, dna);
     int N = dna.length();
-    cout << N << endl;
+    char ha[N], hb[N];
+    char *da, *db;
+    cudaMalloc((void **)&da, N*sizeof(char));
+    cudaMalloc((void **)&db, N*sizeof(char));
+    for (int i = 0; i<N; ++i) {
+        ha[i] = dna[i];
+    }
 }
