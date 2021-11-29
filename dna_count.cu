2335d10b7675b560a7e07f288a7538560890767b
kahnbenjamina <bkahn@chapman.edu>
2021-11-28 22:52:41 -0800

    Testing count.

--- dna_count.cu
+++ dna_count.cu
@@ -8,23 +8,27 @@ using namespace std;
 
 __global__
 void count(char *a, int *b, int N) {
-    int i = blockIdx.x;
-    if (i<N) {
-        if(a[i] == 'A')
-        {
-            b[0]++;
-        }
-        if(a[i] == 'C')
-        {
-            b[1]++;
-        }
-        if(a[i] == 'G')
-        {
-            b[2]++;
-        }
-        if(a[i] == 'T')
-        {
-            b[3]++;
+    int stride = blockDim.x * 2;
+    int tid = threadIdx.x;
+    for(int i=tid+W; i<N; i+=W)
+    {
+        if (i<N) {
+            if(a[i] == 'A')
+            {
+                b[tid]++;
+            }
+            if(a[i] == 'C')
+            {
+                b[tid]++;
+            }
+            if(a[i] == 'G')
+            {
+                b[tid]++;
+            }
+            if(a[i] == 'T')
+            {
+                b[tid]++;
+            }
         }
     }
 }
@@ -37,37 +41,23 @@ int main () {
     inFS.close();
     int N = dna.length();
     char ha[N];
-    int hb[4];
     char *da;
+    int *hb = new int[4];
     int *db;
     cudaMalloc((void **)&da, N*sizeof(char));
-    cudaMalloc((void **)&db, 4*sizeof(int));
+    cudaMalloc((void **)&db, N*sizeof(int));
     for (int i = 0; i<N; ++i) {
         ha[i] = dna[i];
     }
-    for (int i = 0; i < 4; ++i) {
-        hb[i] = 0;
-    }
     cudaMemcpy(da, ha, N*sizeof(char), cudaMemcpyHostToDevice);
+    cudaMemcpy(db, hb, N*sizeof(int), cudaMemcpyHostToDevice);
     int W = 4;
-    count<<<1, W>>>(da, db, N);
+    count<<<1,W>>>(da, db, N);
     cudaDeviceSynchronize();
     int sums[4];
-    cudaMemcpy(sums, db, 4*sizeof(int), cudaMemcpyDeviceToHost);
-    int sum = 0;
-    for(int i = 0; i < W; ++i)
+    cudaMemcpy(sums, da, W*sizeof(int), cudaMemcpyDeviceToHost);
+    for(int i = 0; i < 4; ++i)
     {
-        sums[i] += sums[i];
+        cout << sums[i] << endl;
     }
-    ofstream outFS;
-    outFS.open("output.txt");
-    char letters[] = {'A', 'C', 'G', 'T'};
-    for(int i = 0; i<4; ++i)
-    {
-      outFS << letters[i] <<  " " << sums[i] << endl;
-    }
-    outFS.close();
-    cudaFree(da);
-    cudaFree(db);
-    return 0;
 }
