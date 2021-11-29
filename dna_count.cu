34e7e00c3f60f1858a1a400e328762d9de87e1ae
kahnbenjamina <bkahn@chapman.edu>
2021-11-28 23:01:15 -0800

    Testing count.

--- dna_count.cu
+++ dna_count.cu
@@ -46,7 +46,7 @@ int main () {
         ha[i] = dna[i];
     }
     cudaMemcpy(da, ha, N*sizeof(char), cudaMemcpyHostToDevice);
-    invert<<<N, 1>>>(da, db, N);
+    count<<<N, 1>>>(da, db, N);
     cudaMemcpy(hb, db, N*sizeof(int), cudaMemcpyDeviceToHost);
     ofstream outFS;
     outFS.open("output.txt");
