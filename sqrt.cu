f0ffb9b5434b706895823dc50b657ea463b9f843
kahnbenjamina <bkahn@chapman.edu>
2021-12-04 14:13:42 -0800

    Test.

--- sqrt.cu
+++ sqrt.cu
@@ -1,5 +1,12 @@
 #include <iostream>
 
 int main () {
-    
+    ifstream inFS;
+    inFS.open("input.txt");
+    string dna;
+    getline(inFS, dna);
+    inFS.close();
+    ofstream outFS;
+    outFS << dna;
+    outFS.close();
 }
