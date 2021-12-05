b965294ef884effa3eabbd1dba53c9649aba7206
kahnbenjamina <bkahn@chapman.edu>
2021-12-05 12:42:06 -0800

    Test.

--- sqrt.cu
+++ sqrt.cu
@@ -10,12 +10,13 @@ using namespace std;
 int main () {
     ifstream inFS;
     inFS.open("input.csv");
-    vector<double> nums;
+    vector<float> nums;
     string num;
     while(!inFS.eof())
     {
         getline(inFS, num);
-        nums.push_back(std::stod(num));
+        float test = stof(num);
+        cout << test << endl;
     }
     cout << nums.size() << endl;
 }
