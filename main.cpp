3a479a8a9214ce8a64f399e78eeaf232409b61c4
kahnbenjamina <bkahn@chapman.edu>
2021-12-14 22:39:36 -0800

    test.

--- main.cpp
+++ main.cpp
@@ -120,9 +120,9 @@ void Overlap::makeVectors(int rank) {
             max_min.push_back(min_y);
         }
         inFS.close();
-        for(int i = 0; i < sizes.size(); ++i)
+        for(int i = 0; i < max_min.size(); ++i)
         {
-            cout << sizes[i] << endl;
+            cout << max_min[i] << endl;
         }
     }
 }
