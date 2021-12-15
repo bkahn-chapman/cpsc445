86372fcbbef7a364fe93440d19549eeddf1ebd2c
kahnbenjamina <bkahn@chapman.edu>
2021-12-14 19:15:17 -0800

    Test.

--- main.cu
+++ main.cu
@@ -24,9 +24,12 @@ int main () {
         double max_y = 0;
         double min_y = 0;
         int checkFirst = 0;
-        for(int i = 0; i < line.length(); ++i)
+        for(int i = 1; i < line.length()-1; ++i)
         {
-            
+            if(line[i+1] != '(' && line[i+1] != ',' && line[i+1] != ')')
+            {
+                cout << line[i];
+            }
         }
     }
    
