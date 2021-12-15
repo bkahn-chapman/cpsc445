dc24b5b7e61103537e2aef4117daf8a5a7e7f979
kahnbenjamina <bkahn@chapman.edu>
2021-12-14 22:26:33 -0800

    test.

--- main.cpp
+++ main.cpp
@@ -4,105 +4,124 @@
 #include <vector>
 using namespace std;
 
-int main (int argc, char *argv[]) {
-    int rank;
-    int p;
-    ifstream inFS;
-    inFS.open("input.txt");
-    vector<double> max_min;
-    vector<double> sizes;
-    vector<double> nums;
-    vector<double> overlaps;
-    string line;
-    while(getline(inFS, line))
+class Overlap {
+    public:
+        void makeVectors(int rank);
+        vector<double> max_min;
+        vector<double> sizes;
+        vector<double> nums;
+        vector<double> overlaps;
+};
+
+void Overlap::makeVectors(int rank) {
+    if(rank == 0)
     {
-        string numX;
-        string numY;
-        double max_x = 0;
-        double min_x = 0;
-        double max_y = 0;
-        double min_y = 0;
-        int checkFirstX = 0;
-        int checkFirstY = 0;
-        int XorY = 0;
-        int pairs = 0;
-        for(int i = 0; i < line.length()-1; ++i)
+        ifstream inFS;
+        inFS.open("input.txt");
+        string line;
+        while(getline(inFS, line))
         {
-            if(line[i] == '(')
+            string numX;
+            string numY;
+            double max_x = 0;
+            double min_x = 0;
+            double max_y = 0;
+            double min_y = 0;
+            int checkFirstX = 0;
+            int checkFirstY = 0;
+            int XorY = 0;
+            int pairs = 0;
+            for(int i = 0; i < line.length()-1; ++i)
             {
-                for(int t = i+1; t < line.length(); ++t)
+                if(line[i] == '(')
                 {
-                    if(line[t] == ',')
+                    for(int t = i+1; t < line.length(); ++t)
                     {
-                        if(checkFirstX == 0)
+                        if(line[t] == ',')
                         {
-                            max_x = atof(numX.c_str());
-                            min_x = atof(numX.c_str());
-                            nums.push_back(max_x);
-                            checkFirstX = 1;
+                            if(checkFirstX == 0)
+                            {
+                                max_x = atof(numX.c_str());
+                                min_x = atof(numX.c_str());
+                                nums.push_back(max_x);
+                                checkFirstX = 1;
+                            }
+                            else
+                            {
+                                double testX = atof(numX.c_str());
+                                if(testX > max_x)
+                                {
+                                    max_x = testX;
+                                }
+                                else if(testX < min_x)
+                                {
+                                    min_x = testX;
+                                }
+                                nums.push_back(testX);
+                            }
+                            numX = "";
+                            XorY = 1;
+                        }
+                        else if(line[t] == ')')
+                        {
+                            if(checkFirstY == 0)
+                            {
+                                max_y = atof(numY.c_str());
+                                min_y = atof(numY.c_str());
+                                nums.push_back(max_y);
+                                checkFirstY = 1;
+                            }
+                            else
+                            {
+                                double testY = atof(numY.c_str());;
+                                if(testY > max_y)
+                                {
+                                    max_y = testY;
+                                }
+                                else if(testY < min_y)
+                                {
+                                    min_y = testY;
+                                }
+                                nums.push_back(testY);
+                            }
+                            numY = "";
+                            XorY = 0;
+                            pairs++;
+                            break;
                         }
                         else
                         {
-                            double testX = atof(numX.c_str());
-                            if(testX > max_x)
+                            if(XorY == 0)
                             {
-                                max_x = testX;
+                                numX.push_back(line[t]);
                             }
-                            else if(testX < min_x)
+                            else
                             {
-                                min_x = testX;
+                                numY.push_back(line[t]);
                             }
-                            nums.push_back(testX);
-                        }
-                        numX = "";
-                        XorY = 1;
-                    }
-                    else if(line[t] == ')')
-                    {
-                        if(checkFirstY == 0)
-                        {
-                            max_y = atof(numY.c_str());
-                            min_y = atof(numY.c_str());
-                            nums.push_back(max_y);
-                            checkFirstY = 1;
-                        }
-                        else
-                        {
-                            double testY = atof(numY.c_str());;
-                            if(testY > max_y)
-                            {
-                                max_y = testY;
-                            }
-                            else if(testY < min_y)
-                            {
-                                min_y = testY;
-                            }
-                            nums.push_back(testY);
-                        }
-                        numY = "";
-                        XorY = 0;
-                        pairs++;
-                        break;
-                    }
-                    else
-                    {
-                        if(XorY == 0)
-                        {
-                            numX.push_back(line[t]);
-                        }
-                        else
-                        {
-                            numY.push_back(line[t]);
                         }
                     }
                 }
             }
+            sizes.push_back(pairs);
+            max_min.push_back(max_x);
+            max_min.push_back(min_x);
+            max_min.push_back(max_y);
+            max_min.push_back(min_y);
         }
-        sizes.push_back(pairs);
-        max_min.push_back(max_x);
-        max_min.push_back(min_x);
-        max_min.push_back(max_y);
-        max_min.push_back(min_y);
+        inFS.close();
     }
-    inFS.close();
+}
+
+int main (int argc, char *argv[]) {
+    int rank;
+    int p;
+    
+    Overlap over;
+    // Initialized MPI
+    over.check_error(MPI_Init(&argc, &argv), "unable to initialize MPI");
+    over.check_error(MPI_Comm_size(MPI_COMM_WORLD, &p), "unable to obtain p");
+    over.check_error(MPI_Comm_rank(MPI_COMM_WORLD, &rank), "unable to obtain rank");
+
+    over.makeVectors(rank);
 }
