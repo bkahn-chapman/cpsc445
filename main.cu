4bff6e0076646a20408f323da1cbafd3ae90fcd8
kahnbenjamina <bkahn@chapman.edu>
2021-12-14 18:27:11 -0800

    Test.

--- main.cu
+++ main.cu
@@ -1,3 +1,16 @@
+#include <string>
+
 int main () {
-    
+    ifstream inFS;
+    inFS.open("input.csv");
+    vector<string> polygons;
+    string line;
+    while(getline(inFS, line))
+    {
+        polygons.push_back(line);
+    }
+    for(int i = 0; i < polygons.size(); ++i)
+    {
+        cout << polygons[i] << endl;
+    }
 }
